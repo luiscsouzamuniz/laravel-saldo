f90d6a6a026507c7aac436e787f7c2122801c902
Luis Carlos <luisc.souzamuniz@gmail.com>
2018-11-02 14:39:12 -0300

    Sistema de Saldo

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -80,17 +80,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sistema de Saldo em Laravel
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
     </body>
